fd1e55fe95b49e6ebbf3a64c7298e53e01b06e47
PROPILIL <83790466+PROPILIL@users.noreply.github.com>
2022-05-26 23:29:11 +0300

    Room Database (Database, Dao, TypeConverter, Application.class ovveride) and Repository (as Google said), test database added. So now it's IllegalStateException, because of queries in main thread

--- app/src/main/java/com/example/criminalintent/Crime.kt
+++ app/src/main/java/com/example/criminalintent/Crime.kt
@@ -1,11 +1,13 @@
 package com.example.criminalintent
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
 //UUID - уникальный идентификатор,задаём рандомом
 //Date() - текущая дата по умолчанию
-
-data class Crime(val id: UUID = UUID.randomUUID(),
-    var title: String = "", var date: Date = Date(),
-    var isSolved: Boolean = false) {
+@Entity
+data class Crime(@PrimaryKey val id: UUID = UUID.randomUUID(),
+                 var title: String = "", var date: Date = Date(),
+                 var isSolved: Boolean = false) {
 }
